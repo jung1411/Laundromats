2add9d2e7b8cf653cbadfe9e1a8ac8ecc980d270
Sailesh Sharma <sailesh@Saileshs-MacBook-Air.local>
2021-11-22 17:35:17 -0500

    initialized app and added login screen

---
+++ laundromats/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:laundromats/login_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'Lato',
+      ),
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
+    );
+  }
+}
