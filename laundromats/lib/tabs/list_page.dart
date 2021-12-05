8c53ea7201da370b268a10e32633309e796a2a2c
jung1411 <jung1411@gmail.com>
2021-12-05 15:09:58 -0500

    added listview

--- laundromats/lib/tabs/list_page.dart
+++ laundromats/lib/tabs/list_page.dart
@@ -3,6 +3,7 @@ import 'package:laundromats/register_page.dart';
 import 'package:laundromats/widgets/custom_checkbox.dart';
 import 'package:laundromats/widgets/primary_button.dart';
 import '../theme.dart';
+import 'package:laundromats/listview_result.dart';
 
 class ListPage extends StatefulWidget {
   @override
@@ -10,159 +11,132 @@ class ListPage extends StatefulWidget {
 }
 
 class _ListPage extends State<ListPage> {
-  bool passwordVisible = false;
-  void togglePassword() {
-    setState(() {
-      passwordVisible = !passwordVisible;
-    });
-  }
+  RangeValues _currentRangeValues = const RangeValues(40, 80);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text("List of Laundromats"),
+      ),
       body: SafeArea(
         child: Padding(
-          padding: EdgeInsets.fromLTRB(24.0, 40.0, 24.0, 0),
+          padding: EdgeInsets.fromLTRB(20.0, 30.0, 20.0, 0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(
-                    'Login to your\naccount',
-                    style: heading2.copyWith(color: textBlack),
+                  Text('Location: '),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Form(
+                      child: Column(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                            color: textWhiteGrey,
+                            borderRadius: BorderRadius.circular(14.0)),
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            hintText: 'Use Current Location',
+                            hintStyle: heading6.copyWith(color: textGrey),
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide.none,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  )),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text('Radius (km): '),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Form(
+                      child: Column(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                            color: textWhiteGrey,
+                            borderRadius: BorderRadius.circular(14.0)),
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            hintText: '3',
+                            hintStyle: heading6.copyWith(color: textGrey),
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide.none,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  )),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text('Price Range: '),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  RangeSlider(
+                    values: _currentRangeValues,
+                    min: 0,
+                    max: 100,
+                    divisions: 100,
+                    labels: RangeLabels(
+                      _currentRangeValues.start.round().toString(),
+                      _currentRangeValues.end.round().toString(),
+                    ),
+                    onChanged: (RangeValues values) {
+                      setState(() {
+                        _currentRangeValues = values;
+                      });
+                    },
                   ),
                   SizedBox(
                     height: 20,
                   ),
-                  Image.asset(
-                    'assets/images/accent.png',
-                    width: 99,
-                    height: 4,
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 48,
-              ),
-              Form(
-                child: Column(
-                  children: [
-                    Container(
+                  Material(
+                    borderRadius: BorderRadius.circular(14.0),
+                    elevation: 0,
+                    child: Container(
+                      height: 56,
                       decoration: BoxDecoration(
-                        color: textWhiteGrey,
+                        color: primaryBlue,
                         borderRadius: BorderRadius.circular(14.0),
                       ),
-                      child: TextFormField(
-                        decoration: InputDecoration(
-                          hintText: 'Email',
-                          hintStyle: heading6.copyWith(color: textGrey),
-                          border: OutlineInputBorder(
-                            borderSide: BorderSide.none,
+                      child: Material(
+                        color: Colors.transparent,
+                        child: InkWell(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => ListViewResult()));
+                          },
+                          borderRadius: BorderRadius.circular(14.0),
+                          child: Center(
+                            child: Text(
+                              'Results',
+                              style: heading5.copyWith(color: Colors.white),
+                            ),
                           ),
                         ),
                       ),
                     ),
-                    SizedBox(
-                      height: 32,
-                    ),
-                    Container(
-                      decoration: BoxDecoration(
-                        color: textWhiteGrey,
-                        borderRadius: BorderRadius.circular(14.0),
-                      ),
-                      child: TextFormField(
-                        obscureText: !passwordVisible,
-                        decoration: InputDecoration(
-                          hintText: 'Password',
-                          hintStyle: heading6.copyWith(color: textGrey),
-                          suffixIcon: IconButton(
-                            color: textGrey,
-                            splashRadius: 1,
-                            icon: Icon(passwordVisible
-                                ? Icons.visibility_outlined
-                                : Icons.visibility_off_outlined),
-                            onPressed: togglePassword,
-                          ),
-                          border: OutlineInputBorder(
-                            borderSide: BorderSide.none,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 32,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  CustomCheckbox(),
-                  SizedBox(
-                    width: 12,
-                  ),
-                  Text('Remember me', style: regular16pt),
-                ],
-              ),
-              SizedBox(
-                height: 32,
-              ),
-              CustomPrimaryButton(
-                buttonColor: primaryBlue,
-                textValue: 'Login',
-                textColor: Colors.white,
-              ),
-              SizedBox(
-                height: 24,
-              ),
-              Center(
-                child: Text(
-                  'OR',
-                  style: heading6.copyWith(color: textGrey),
-                ),
-              ),
-              SizedBox(
-                height: 24,
-              ),
-              CustomPrimaryButton(
-                buttonColor: Color(0xfffbfbfb),
-                textValue: 'Login with Google',
-                textColor: textBlack,
-              ),
-              SizedBox(
-                height: 50,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Don't have an account? ",
-                    style: regular16pt.copyWith(color: textGrey),
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => RegisterPage()));
-                    },
-                    child: Text(
-                      'Register',
-                      style: regular16pt.copyWith(color: primaryBlue),
-                    ),
                   ),
                 ],
-              ),
+              )
             ],
           ),
         ),
       ),
     );
-    ;
   }
 }
